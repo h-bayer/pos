147c495c3c6c3d8e6cc02c7bdc0af024a9953c04
harald.bayer <h.bayer@phoenixgroup.eu>
2025-09-30 16:59:01 +0200

    configured infra for kafka

---
+++ inventory-service/src/main/java/de/bayer/pharmacy/inventoryservice/config/Configuration.java
@@ -0,0 +1,18 @@
+package de.bayer.pharmacy.inventoryservice.config;
+
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.config.TopicBuilder;
+
+@org.springframework.context.annotation.Configuration
+public class Configuration {
+    @Bean
+    public NewTopic productEventsTopic(@Value("${app.topics.productEvents}") String name) {
+        return TopicBuilder.name(name)
+                .partitions(6)
+                .replicas(1)
+                .compact()   // oder delete, retention, configs etc.
+                .build();
+    }
+}
